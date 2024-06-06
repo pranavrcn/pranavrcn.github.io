b5f862b74e2c8d310686f2197446a7869d07e039
PranavRavichandran2002 <121128196+PranavRavichandran2002@users.noreply.github.com>
2024-06-06 19:11:45 -0400

    Changes

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to My GitHub Portfolio</h1>
+      <p>Hello! I'm [Your Name], a passionate developer.</p>
+      <p>Current Activities:</p>
+      <ul>
+        <li>Working on cool projects.</li>
+        <li>Learning new technologies.</li>
+        <li>Contributing to open source.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
